035d0cb72793f7631afffb647fc6c63fee7ae90a
Gina Park <koyrkr@gmail.com>
2021-11-18 00:54:09 +0900

    feat: add FlexScreen for flex tutorials

--- doEverything/App.js
+++ doEverything/App.js
@@ -1,24 +1,29 @@
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import * as React from 'react';
+import {Button, Text, View} from 'react-native';
+import FlexScreen from './src/pages/FlexScreen';
 
-function HomeScreen() {
+function HomeScreen({navigation}) {
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
       <Text>Home Screen</Text>
+      <Button
+        title="Go to Flex Screen"
+        onPress={() => navigation.navigate('Flex')}
+      />
     </View>
   );
 }
- 
- export default function App() {
-  const Stack = createNativeStackNavigator();
-   return (
-     <NavigationContainer>      
-       <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
-      </Stack.Navigator>
-   </NavigationContainer>
-   );
- }
 
-export default App;
+export default function App() {
+  const Stack = createNativeStackNavigator();
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Flex" component={FlexScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
